e2ceb0acc45590880764f80fd9e6bd1af9e1fe7c
danla <dan.lawrence.walker@gmail.com>
2023-10-05 14:01:27 -0700

    Updated User entity data

--- database/create_user_table.sql
+++ database/create_user_table.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS `user` (
     `provider_id` VARCHAR(256) NOT NULL COMMENT 'OAuth2 provider ID',
     `provider_name` VARCHAR(256) NOT NULL COMMENT 'OAuth2 provider name',
     `email_id` VARCHAR(256) DEFAULT NULL COMMENT 'Optionally provided contact email',
-    `last_login` DATETIME DEFAULT NULL COMMENT 'Datetimestamp representing the last system access',
+    `last_login_dt` DATETIME DEFAULT NULL COMMENT 'Datetimestamp representing the last system access',
     `created_dt` DATETIME NOT NULL COMMENT 'Datetimestamp representing the user registration',
     `last_updated_dt` DATETIME DEFAULT NULL COMMENT 'Datetimetamp representing the last record update',
     PRIMARY KEY (`id`)
--- sprout-pass/src/main/java/dev/danwalkercs/sproutpass/domain/data/User.java
+++ sprout-pass/src/main/java/dev/danwalkercs/sproutpass/domain/data/User.java
@@ -1,7 +1,30 @@
 package dev.danwalkercs.sproutpass.domain.data;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String providerId;
+    private String providerName;
+    private String emailId;
+    private LocalDate lastLogin;
+    private LocalDate createdDt;
+    private LocalDate lastUpdatedDt;
 }
