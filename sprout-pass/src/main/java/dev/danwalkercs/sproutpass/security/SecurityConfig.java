40348a583cf4b4b54454a2ba4cd94bac21158348
danla <dan.lawrence.walker@gmail.com>
2023-10-08 14:31:19 -0700

    Updated default success URL for oauth2 login

--- sprout-pass/src/main/java/dev/danwalkercs/sproutpass/security/SecurityConfig.java
+++ sprout-pass/src/main/java/dev/danwalkercs/sproutpass/security/SecurityConfig.java
@@ -4,9 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -14,12 +13,15 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
+        http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests( auth -> {
-                    auth.requestMatchers("/open/*").permitAll();
-                    auth.requestMatchers("/secure/*").authenticated();
+                    auth.requestMatchers("/open/**").permitAll();
+                    auth.requestMatchers("/secure/**").authenticated();
                 })
-                .oauth2Login(withDefaults())
-                .build();
+                .oauth2Login(oauth2Login -> {
+                    oauth2Login.defaultSuccessUrl("/secure/user/current", true);
+                });
+        return http.build();
     }
 }
