3986da9cc6d4f30dc779832817bf842d93e0ca8a
danla <dan.lawrence.walker@gmail.com>
2023-10-09 13:01:24 -0700

    Added logic to conditionally populate audit information

--- sprout-pass/src/main/java/dev/danwalkercs/sproutpass/domain/AbstractDomainEntity.java
+++ sprout-pass/src/main/java/dev/danwalkercs/sproutpass/domain/AbstractDomainEntity.java
@@ -5,21 +5,29 @@ import lombok.*;
 
 import java.io.Serializable;
 import java.time.ZonedDateTime;
+import java.util.Objects;
 
 @MappedSuperclass
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class AbstractDomainEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @NonNull
     protected Long id;
 
     @Temporal(TemporalType.TIMESTAMP)
-    @NonNull
     private ZonedDateTime createdDt;
 
     @Temporal(TemporalType.TIMESTAMP)
     private ZonedDateTime lastUpdatedDt;
+
+    public void auditUpdate(ZonedDateTime now) {
+        if (Objects.isNull(id)) {
+            this.createdDt = now;
+        } else {
+            this.lastUpdatedDt = now;
+        }
+    }
 }
