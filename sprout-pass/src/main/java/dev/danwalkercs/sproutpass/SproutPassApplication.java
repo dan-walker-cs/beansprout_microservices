0fdedf9efadf48d2f210ddf7b0fe53649f190f8e
danla <dan.lawrence.walker@gmail.com>
2023-10-11 08:15:48 -0700

    Upgraded springboot version from 3.1.3 to 3.1.4

--- sprout-pass/src/main/java/dev/danwalkercs/sproutpass/SproutPassApplication.java
+++ sprout-pass/src/main/java/dev/danwalkercs/sproutpass/SproutPassApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SproutPassApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SproutPassApplication.class, args);
 	}
-
 }
