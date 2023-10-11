9c3d6fc66d0c68f19916f7e04a6ceedffc26f094
danla <dan.lawrence.walker@gmail.com>
2023-10-11 13:06:33 -0700

    Added module for spring cloud gateway w/ configuration -- currently discoverable by Eureka, but routing is busto

---
+++ sprout-route/src/test/java/dev/danwalkercs/sproutroute/SproutRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danwalkercs.sproutroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SproutRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
