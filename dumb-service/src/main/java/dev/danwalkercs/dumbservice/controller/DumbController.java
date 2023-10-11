b5fa505e96cd69645699fe895ad67b098cdd28db
danla <dan.lawrence.walker@gmail.com>
2023-10-11 10:49:27 -0700

    Discovery service boilerplate and dummy services configured as discovery clients

--- dumb-service/src/main/java/dev/danwalkercs/dumbservice/controller/DumbController.java
+++ dumb-service/src/main/java/dev/danwalkercs/dumbservice/controller/DumbController.java
@@ -1,13 +1,28 @@
 package dev.danwalkercs.dumbservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DumbController {
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/")
     public String getDumb() {
         return "Hello, from the DumbService!";
+    }
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
     }
 }
---
+++ sprout-seek/src/test/java/dev/danwalkercs/sproutseek/SproutSeekApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danwalkercs.sproutseek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SproutSeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
